220fa4edc49b3f8e35dd908cf9f93c94f1dfa245
Cole Flournoy <flournoy.cole@gmail.com>
2021-01-15 15:38:34 -0500

    commit tree 0230108f75eafcbab19de590b5caaf4ed6a62cdc

---
+++ lib/insert.sql
@@ -0,0 +1,58 @@
+INSERT INTO users (name, age) VALUES ("Peter", 5); 
+INSERT INTO users (name, age) VALUES ("Paul", 4);
+INSERT INTO users (name, age) VALUES ("Patrick", 6);
+INSERT INTO users (name, age) VALUES ("Sandy", 7);
+INSERT INTO users (name, age) VALUES ("Susan", 12);
+INSERT INTO users (name, age) VALUES ("Steve", 32);
+INSERT INTO users (name, age) VALUES ("Sandra", 43);
+INSERT INTO users (name, age) VALUES ("Bob", 54);
+INSERT INTO users (name, age) VALUES ("Betty", 65);
+INSERT INTO users (name, age) VALUES ("Boop", 76);
+INSERT INTO users (name, age) VALUES ("Bain", 32);
+INSERT INTO users (name, age) VALUES ("Batman", 21);
+INSERT INTO users (name, age) VALUES ("Bubble Boy", 24);
+INSERT INTO users (name, age) VALUES ("Biff", 42);
+INSERT INTO users (name, age) VALUES ("Beatrice", 71);
+INSERT INTO users (name, age) VALUES ("Bonnie", 41);
+INSERT INTO users (name, age) VALUES ("Al", 11);
+INSERT INTO users (name, age) VALUES ("Andrew", 22);
+INSERT INTO users (name, age) VALUES ("Alice", 33);
+INSERT INTO users (name, age) VALUES ("Alexa", 44);
+
+INSERT INTO projects (title) 
+VALUES ("oop"), ("loop"), ("boop"), ("troop"), ("scoop"), ("floop"), ("bloop"), ("coop"), ("droop"), ("soup");
+
+INSERT INTO pledges (amount)
+VALUES
+    (5), 
+    (6),
+    (33), 
+    (11),
+    (22), 
+    (44),
+    (55),
+    (66),
+    (77),
+    (12),
+    (13),
+    (14),
+    (88),
+    (99),
+    (101),
+    (31),
+    (32),
+    (34),
+    (35),
+    (36),
+    (37),
+    (81),
+    (82),
+    (83),
+    (84),
+    (85),
+    (86),
+    (98),
+    (97),
+    (96);
+
+    
